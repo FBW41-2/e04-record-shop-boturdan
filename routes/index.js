b5b2e8322d11bc0cc2f90d6932354db519f23753
boturdan <eric.solo@gmail.com>
2021-04-20 14:15:55 +0200

    Task #1 Dan Done!

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const records = [
+  { "name": "Metallica",
+    "title": "Unforgiven",
+    "year": "1998"
+  },
+
+  { "name": "Beatles",
+    "title": "Let It Be",
+    "year": "1975"
+  },
+
+  { "name": "System Of A Down",
+    "title": "Chop Suey",
+    "year": "2002"
+  }
+]
+
+
+router.get('/api/records', function(req, res) {
+  res.json(records)
+})
+
+router.post('/api/records', (req, res) => {
+  
+  records.push({
+
+    name: req.body.name,
+    title: req.body.title,
+    year: req.body.year,
+  
+  });
+  
+  res.redirect("/api/records");
+});
+
 module.exports = router;
